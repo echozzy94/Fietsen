c5390cacf8dc468c7dc5af23e7ebc3732e765117
Echozzy94 <bryantvincken@gmail.com>
2021-11-25 20:44:10 +0100

    Ook secondes weergegeven

--- Fietstracker.py
+++ Fietstracker.py
@@ -8,13 +8,17 @@ from colorama import init, Fore, Back, Style
 avgkcalburned = 560
 totalkcalburned = 0
 minutes = 0
+seconds = 0
 
 print(Fore.GREEN + "###############")
 print(Fore.GREEN + "Welkom fietsboi")
 print(Fore.GREEN + "###############" + Style.RESET_ALL)
 
 while True:
-    time.sleep(60.0)
-    totalkcalburned += (avgkcalburned / 60 )
-    minutes += 1
-    print(Fore.BLUE + "Tijd op fiets is " + str(minutes) + " minuten. Totaal aantal kcal verbrand is: " + str(round(totalkcalburned, 1)))
+    time.sleep(1.0)
+    totalkcalburned += (avgkcalburned / 60 / 60)
+    seconds += 1
+    if seconds == 60:
+        seconds = 0
+        minutes += 1
+    print(Fore.BLUE + "Tijd op fiets is " + str(minutes) + " minuten en " + str(seconds) + " seconden. Totaal aantal kcal verbrand is: " + str(round(totalkcalburned, 1)))
